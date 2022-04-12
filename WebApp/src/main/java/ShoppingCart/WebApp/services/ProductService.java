f78e430d56474cff966b5a175e99683e4d2b240d
Sahil Gupta <thestrandedalien.mysticdark@gmail.com>
2022-04-12 22:03:44 +0530

    Implementing Product Service

--- WebApp/src/main/java/ShoppingCart/WebApp/services/ProductService.java
+++ WebApp/src/main/java/ShoppingCart/WebApp/services/ProductService.java
@@ -1,4 +1,14 @@
 package ShoppingCart.WebApp.services;
 
+import ShoppingCart.WebApp.models.Product;
+
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getAllProducts();
+    void saveProduct(Product product);
+    Product updateProduct(Product product);
+    Product getProductById(Long id);
+    void deleteProductById(Long id);
 }
--- WebApp/src/main/java/ShoppingCart/WebApp/services/implementations/ProductServiceImpl.java
+++ WebApp/src/main/java/ShoppingCart/WebApp/services/implementations/ProductServiceImpl.java
@@ -1,8 +1,57 @@
 package ShoppingCart.WebApp.services.implementations;
 
+import ShoppingCart.WebApp.models.Product;
+import ShoppingCart.WebApp.repositories.ProductRepository;
 import ShoppingCart.WebApp.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public List<Product> getAllProducts() {
+        return this.productRepository.findAll();
+    }
+
+    @Override
+    public void saveProduct(Product product) {
+        this.productRepository.save(product);
+    }
+
+    @Override
+    public Product updateProduct(Product product) {
+        Optional<Product> optional = this.productRepository.findById(product.getId());
+        if(optional.isPresent()) {
+            Product existingProduct = optional.get();
+            existingProduct.setProductName(product.getProductName());
+            existingProduct.setUnitPrice(product.getUnitPrice());
+            existingProduct.setSalesTaxRate(product.getSalesTaxRate());
+            existingProduct.setQuantityAvailable(product.getQuantityAvailable());
+            return this.productRepository.save(existingProduct);
+        }
+        throw new RuntimeException("Product Not Found !");
+    }
+
+    @Override
+    public Product getProductById(Long id) {
+        Optional<Product> optional = this.productRepository.findById(id);
+        Product product = null;
+        if(optional.isPresent()) {
+            product = optional.get();
+        } else {
+            throw new RuntimeException("Product with Id : " + id + " Not Found !");
+        }
+        return product;
+    }
+
+    @Override
+    public void deleteProductById(Long id) { this.productRepository.deleteById(id); }
+
 }
