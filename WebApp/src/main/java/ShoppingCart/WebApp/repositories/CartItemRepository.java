6d4b38324060b953a83ccd90735dc139c81f8aa8
Sahil Gupta <thestrandedalien.mysticdark@gmail.com>
2022-04-12 19:43:19 +0530

    Setting up JPA Repositories for all Entities

---
+++ WebApp/src/main/java/ShoppingCart/WebApp/repositories/CartItemRepository.java
@@ -0,0 +1,9 @@
+package ShoppingCart.WebApp.repositories;
+
+import ShoppingCart.WebApp.models.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+}
---
+++ WebApp/src/main/java/ShoppingCart/WebApp/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package ShoppingCart.WebApp.repositories;
+
+import ShoppingCart.WebApp.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
