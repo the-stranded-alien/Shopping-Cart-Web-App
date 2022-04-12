f2c727b35627f3c0816f93d9c405c9843d022473
Sahil Gupta <thestrandedalien.mysticdark@gmail.com>
2022-04-12 19:23:40 +0530

    Setting up Product Entity

---
+++ WebApp/src/main/java/ShoppingCart/WebApp/models/Product.java
@@ -0,0 +1,74 @@
+package ShoppingCart.WebApp.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Product implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String productName;
+    private Double unitPrice;
+    private Double salesTaxRate;
+    private Integer quantityAvailable;
+
+    public Product() {
+
+    }
+
+    public Product(String productName, Double unitPrice, Double salesTaxRate, Integer quantityAvailable) {
+        this.productName = productName;
+        this.unitPrice = unitPrice;
+        this.salesTaxRate = salesTaxRate;
+        this.quantityAvailable = quantityAvailable;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public Double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public Double getSalesTaxRate() {
+        return salesTaxRate;
+    }
+
+    public Integer getQuantityAvailable() {
+        return quantityAvailable;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product)) return false;
+        Product other = (Product) o;
+        return (id != null && id.equals(other.getId()));
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productName='" + productName + '\'' +
+                ", unitPrice=" + unitPrice +
+                ", salesTaxRate=" + salesTaxRate +
+                ", quantityAvailable=" + quantityAvailable +
+                '}';
+    }
+}
