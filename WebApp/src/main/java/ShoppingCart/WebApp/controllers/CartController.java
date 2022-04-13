92ade4dd33b8dff0fbe158f57567ce1e62ec0178
Sahil Gupta <thestrandedalien.mysticdark@gmail.com>
2022-04-13 16:18:35 +0530

    Defining Cart Controller

--- WebApp/src/main/java/ShoppingCart/WebApp/controllers/CartController.java
+++ WebApp/src/main/java/ShoppingCart/WebApp/controllers/CartController.java
@@ -1,9 +1,49 @@
 package ShoppingCart.WebApp.controllers;
 
+import ShoppingCart.WebApp.models.Cart;
+import ShoppingCart.WebApp.models.CartItem;
+import ShoppingCart.WebApp.models.Product;
+import ShoppingCart.WebApp.services.CartItemService;
+import ShoppingCart.WebApp.services.CartService;
+import ShoppingCart.WebApp.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/cart")
 public class CartController {
+
+    @Autowired
+    private CartService cartService;
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/{id}")
+    public String viewCart(@PathVariable(value="id") Long id, Model model) {
+        Cart cart = this.cartService.getCartById(id);
+        model.addAttribute("cart", cart);
+        return "cart/home";
+    }
+
+    @GetMapping("/{id}/addItem")
+    public String addCartItem(@PathVariable(value = "id") Long id, Model model) {
+        CartItem cartItem = new CartItem();
+        Cart cart = this.cartService.getCartById(id);
+        List<Product> products = this.productService.getAllProducts();
+        model.addAttribute("products", products);
+        model.addAttribute("cart", cart);
+        model.addAttribute("cartItem", cartItem);
+        return "cart/newItem";
+    }
+
+    @PostMapping("/{id}/saveItem")
+    public String saveCartItem(@PathVariable(value = "id") Long id, @ModelAttribute("cartItem") @RequestBody CartItem cartItem) {
+        this.cartService.saveCartItemToCart(id, cartItem);
+        return "redirect:/cart/{id}";
+    }
 }
--- WebApp/src/main/java/ShoppingCart/WebApp/controllers/MainController.java
+++ WebApp/src/main/java/ShoppingCart/WebApp/controllers/MainController.java
@@ -1,13 +1,20 @@
 package ShoppingCart.WebApp.controllers;
 
+import ShoppingCart.WebApp.models.Cart;
+import ShoppingCart.WebApp.services.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
+    @Autowired
+    private CartService cartService;
+
     @GetMapping("/")
     public String root() {
+        this.cartService.generateCart();
         return "index";
     }
 }
